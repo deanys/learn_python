abc374f68cc2322ca73090f76188660e8007f461
deanys <1918047503@qq.com>
2019-08-30 20:43:14 +0800

    add wifi test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learn_python
 learn_myself
+wifi_test item:
+主要用来练手，局限性比较大。无字典，不能破解中文名字的wifi
+目前下一步打算学习爬虫。。。。。。
--- wifi_test/pywifi_test.py
+++ wifi_test/pywifi_test.py
@@ -1,13 +1,52 @@
 import pywifi
 from pywifi import const
+import time
 
-wifi = pywifi.PyWiFi()
-interface = wifi.interfaces()[0]
-print(interface, type(interface))
-# if interface in [const.IFACE_DISCONNECTED,
-#               const.IFACE_INACTIVE]:
-# print('已连接')
-if interface.status() == const.IFACE_CONNECTED:
-    print("wifi has been bbconnect")
-else:
-    print("wifi no connect")
+
+class WIFICON():
+    inter = ""
+
+    def __init__(self, wifi_name):
+        wifi = pywifi.PyWiFi()
+        self.inter = wifi.interfaces()[0]
+        self.wifi_name = wifi_name
+
+    def wifi_status_check(self):
+        if self.inter.status() == const.IFACE_CONNECTED:
+            # print("wifi has been connect")
+            return True
+        else:
+            # print("wifi no connect")
+            return False
+
+    def wifi_connect(self, password):
+        print("wifi name is [{}],password is [{}]".format(self.wifi_name, password.strip()))
+        profile = pywifi.Profile()
+        profile.ssid = self.wifi_name
+        profile.key = password
+        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        profile.auth = const.AUTH_ALG_OPEN
+        profile.cipher = const.CIPHER_TYPE_CCMP
+        self.inter.remove_all_network_profiles()
+        temfile = self.inter.add_network_profile(profile)
+        self.inter.connect(temfile)
+        time.sleep(5)
+
+    def read_password_note(self):
+        with open("pass.txt", "r+") as f:
+            return f.readlines()
+
+    def wifi_test(self):
+        if self.wifi_status_check():
+            print("wifi has been conect ,no need connect")
+        for password in self.read_password_note():
+            self.wifi_connect(password.strip())
+            if self.wifi_status_check():
+                # print("good,wifi connect ,wifi name is [{}],password is [{}]".format(self.wifi_name, password.strip()))
+                return
+        print("wifi no connect,password use all,sorry!")
+
+
+if __name__ == '__main__':
+    wif = WIFICON("test")
+    wif.wifi_test()
