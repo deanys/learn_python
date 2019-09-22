029314a3691e2667bdb85dca3c805fe517e517bb
deanys <1918047503@qq.com>
2019-09-22 18:55:25 +0800

    add wifi test

---
+++ built/learn_built.py
@@ -0,0 +1,7 @@
+# -*- coding=utf8
+s = "1+2"
+s="ashhd jjdjjf kkkd .ff..fgt.."
+print(s)
+print(s.replace(".","_"))
+# print(eval(s))
+print(repr(s).replace(".", "_"))
--- spide_crawler/first_web_data.py
+++ spide_crawler/first_web_data.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-html=urlopen("http://fund.eastmoney.com/001595.html?spm=haosou")
-hs=BeautifulSoup(html.read())
-print(hs)
+
+html = urlopen(
+    "https://temai.taobao.com/cheap.htm?spm=a231o.7076277%2Fb.19985674835.1.50aa46089wWDYM&prepvid=200_11.15.192.192_465_1567171979704&extra=&pid=mm_26632323_6762370_25910879&clk1=&unid=&source_id=&app_pvid=200_11.15.192.192_465_1567171979704")
+# f = open("web-code.txt", "w")
+# f.write(str(html.read()))
+# f.close()
+# s=f.read()
+# print(s)
+hs = BeautifulSoup(html.read())
+print(hs.title)
